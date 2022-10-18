dd609e5d3b93c5a5abf47a405704c9062de1227e
Chugaister <dmytroshvahyliak@gmail.com>
2022-10-18 23:13:38 +0300

    first

---
+++ p2/p2.cpp
@@ -0,0 +1,18 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+
+int main()
+{
+	double a, b, z1, z2;
+	std::cout << "Input alpha > ";s
+	std::cin >> a;
+	std::cout << "Input betha > ";
+	std::cin >> b;
+	a *= M_PI / 180;
+	b *= M_PI / 180;
+	z1 = pow(cos(a) - cos(b), 2) - pow(sin(a) - sin(b), 3);
+	z2 = -4 * pow(sin((a - b) / 2), 2) * cos(a + b);
+	std::cout << "z1 = " << z1 << "\n";
+	std::cout << "z1 = " << z2 << "\n";
+
+}
